a36c13b13909fec2bee2ff22719edf988d021580
PrimeEagle <misc@hiddendragon.com>
2023-05-27 18:57:14 +0000

    git-svn-id: https://matrix.tplinkdns.com:8443/svn/MyCode/VNet/VNet.DataStructures@1109 0f8fcb7d-4e12-9a4c-a6b3-2570e6efd5fd

--- VNet.DataStructures/Algorithms/Search/Interpolation.cs
+++ VNet.DataStructures/Algorithms/Search/Interpolation.cs
@@ -1,4 +1,4 @@
-﻿namespace VNet.DataStructures.Algorithms.Search;
+﻿namespace VNet.DataStructures.List.Algorithms.Search;
 
 // Interpolation search is an improved variant of binary search.
 // This search algorithm works on the probing position of the required value. For this algorithm to work properly, the data collection should be in sorted and equally distributed form.
