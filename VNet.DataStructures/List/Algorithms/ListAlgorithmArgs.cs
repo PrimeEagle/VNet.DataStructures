72beae75db1166648c3afe6a13fe6654b1581329
PrimeEagle <misc@hiddendragon.com>
2023-05-28 05:10:45 +0000

    git-svn-id: https://matrix.tplinkdns.com:8443/svn/MyCode/VNet/VNet.DataStructures@1113 0f8fcb7d-4e12-9a4c-a6b3-2570e6efd5fd

---
+++ VNet.DataStructures/List/Algorithms/ListAlgorithmArgs.cs
@@ -0,0 +1,18 @@
+﻿namespace VNet.DataStructures.List.Algorithms
+{
+    public class ListAlgorithmArgs<T> : IListAlgorithmArgs<T>
+                                        where T : notnull
+    {
+        public IList<T> List { get; set; }
+
+        public ListAlgorithmArgs()
+        {
+            List = new List<T>();
+        }
+
+        public ListAlgorithmArgs(IList<T> list)
+        {
+            List = list;
+        }
+    }
+}
