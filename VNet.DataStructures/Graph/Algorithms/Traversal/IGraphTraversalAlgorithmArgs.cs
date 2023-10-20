3db7e3c56f1e606f2dfdc2e85faa66c9aba54bc8
PrimeEagle <misc@hiddendragon.com>
2023-10-20 20:52:13 +0000

    git-svn-id: https://matrix.tplinkdns.com:8443/svn/MyCode/VNet/VNet.DataStructures@1414 0f8fcb7d-4e12-9a4c-a6b3-2570e6efd5fd

--- VNet.DataStructures/Graph/Algorithms/Traversal/IGraphTraversalAlgorithmArgs.cs
+++ VNet.DataStructures/Graph/Algorithms/Traversal/IGraphTraversalAlgorithmArgs.cs
@@ -6,7 +6,7 @@
                                                                           where TValue : notnull, IComparable<TValue>
     {
         public TNode StartNode { get; set; }
-        public TNode EndNode { get; set; }
+        public TNode? EndNode { get; set; }
         public Action<TNode>? OnVisitNode { get; set; }
         public Action<TNode>? OnVisitedNode { get; set; }
         public Func<TNode, bool>? ShouldStop { get; set; }
